d333caff495545459aceff798765d72c07cf9cae
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-12-18 13:43:50 +0100

    Separated the form section in the BlogForm component from the MainComponent component

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,65 +1,5 @@
-import { useState } from "react";
-
-const initialPost = {
-    id: "",
-    title: "",
-    image: "https://picsum.photos/640/480",
-    content: "",
-    published: false,
-};
-
-function BlogForm() {
-    // Variabile di stato dei post, che conterrà un oggetto dei post
-    const [post, setPost] = useState(initialPost);
-    // Variabile di stato della lista di post
-    const [postList, setPostList] = useState([]);
-
-    // Funzione che gestisce gli eventi sulle Input text
-    function handlerInput(event) {
-        /* Metodo lungo: */
-        // const newPost = { ...post };    // Mi creo una copia dell'oggetto post
-        // newPost[event.target.name] = event.target.value;    //Gli assegno ogni suo corrispettivo valore
-        // setPost(newPost);   // Setto la variabile oggetto di stato
-
-        // Alla costante value assegno il valore "true" (con event.target.checked) se è checkata altrimenti lascio quello di default "false" e procedo con le assegnazioni di altri valori delle Input
-        const value = event.target.type === "checkbox" ? event.target.checked : event.target.value;
-
-        let newId;
-        if (postList.length === 0) {
-            newId = 1;
-        }
-        else {
-            const objectId = postList.reduce((previous, next) => {
-                return next.id > previous.id ? next : previous;
-            });
-            newId = objectId.id;
-            newId++;
-        }
-
-        /* Metodo breve: */
-        // Creo prima una copia dell'oggetto posto con il rest operator ...post
-        // poi assegno alla rispettiva chiave (cercata dinamicamente in base al valore
-        // contenuto in [event.target.name]) il valore assegnato al controllo precedente a value
-        setPost({ ...post, id: newId, [event.target.name]: value });
-        // setPost([...post, { id: newId, title: event.target.value }]);
-
-    }
-
-    // Funzione che gestisce l'evento al click sul pulsante di invio
-    function handlerSubmit(event) {
-        event.preventDefault();     //blocco il caricamento della pagina
-        // /* Metodo lungo (come sopra): */
-        // const newPostList = {...postList};
-        // newPostList.push(post);
-        // setPostList(newPostList);
-
-        /* Metodo breve (come sopra): */
-        setPostList([...postList, post]);
-
-        // Svuoto il form dai valori inseriti dopo il click al pulsante di submit
-        setPost(initialPost);
-    }
-
+// Funzione che disegna il form per l'inserimento dati
+function BlogForm({ handlerInput, handlerSubmit, postList }) {
     //parte html da ritornare
     return (
         <section className="my-4">
@@ -74,7 +14,7 @@ function BlogForm() {
                         className="form-control"
                         id="title"
                         aria-describedby="titlelHelp"
-                        value={post.title}
+                        value={postList.title}
                         onChange={handlerInput}
                         name="title"
                     />
@@ -90,7 +30,7 @@ function BlogForm() {
                         className="form-control"
                         id="content"
                         rows="5"
-                        value={post.content}
+                        value={postList.content}
                         onChange={handlerInput}
                         name="content">
                     </textarea>
--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Card from "./Card";
+import BlogForm from "./BlogForm";          // Importo il componente BlogForm che disegnerà i campi del form in questo componente
 
+// Variabile usata per inizializzare lo useState post e successivamente per svuotare i campi del form
 const initialPost = {
     id: "",
     title: "",
@@ -33,6 +35,7 @@ export default function MainComponent() {
         // Alla costante value assegno il valore "true" (con event.target.checked) se è checkata altrimenti lascio quello di default "false" e procedo con le assegnazioni di altri valori delle Input
         const value = event.target.type === "checkbox" ? event.target.checked : event.target.value;
 
+        /* -------- Sezione che calcola l'id univoco dell'oggetto assegnandogli il valore dell'id dell'ultimo oggetto + 1 -------- */
         let newId;
         if (postList.length === 0) {
             newId = 1;
@@ -44,6 +47,7 @@ export default function MainComponent() {
             newId = objectId.id;
             newId++;
         }
+        /* --------------------------------------------- Fine sezione che calcolo id --------------------------------------------- */
 
         /* Metodo breve: */
         // Creo prima una copia dell'oggetto posto con il rest operator ...post
@@ -87,58 +91,12 @@ export default function MainComponent() {
                         onDelete={() => deletedPost(post.id)} />
                 }
                 )}
-
-            < section className="my-4" >
-                <h2>Inserisci un articolo del blog</h2>
-                <form onSubmit={handlerSubmit}>
-                    <div className="mb-3">
-                        <label htmlFor="title" className="form-label">
-                            Titolo del post
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="title"
-                            aria-describedby="titlelHelp"
-                            value={post.title}
-                            onChange={handlerInput}
-                            name="title"
-                        />
-                        <div id="titlelHelp" className="form-text">
-                            Il tuo articolo sarà pubblicato nel nostro blog!
-                        </div>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="content" className="form-label">
-                            Contenuto dell'articolo
-                        </label>
-                        <textarea
-                            className="form-control"
-                            id="content"
-                            rows="5"
-                            value={post.content}
-                            onChange={handlerInput}
-                            name="content">
-                        </textarea>
-                    </div>
-
-                    <div className="mb-3 form-check">
-                        <input
-                            type="checkbox"
-                            className="form-check-input"
-                            id="published"
-                            name="published"
-                            onChange={handlerInput}
-                        />
-                        <label className="form-check-label" htmlFor="published">
-                            Clicca qui se vuoi pubblicare subito, altrimenti lascialo in bozza
-                        </label>
-                    </div>
-                    <button type="submit" className="btn btn-primary">
-                        Inserisci articolo
-                    </button>
-                </form>
-            </section >
+            
+            <BlogForm 
+            handlerInput={handlerInput} 
+            handlerSubmit={handlerSubmit } 
+            postList={postList} 
+            />
         </>
     );
 }
